27d2f4a50783508a57d18d2dcd8109bb75068b12
stephen jobs <stephenjobs@stephens-Mac-mini.local>
2026-03-14 15:07:17 -0400

    Update cars-for-sale: 2025 Aston Martin DBX

--- src/data/cars-for-sale.ts
+++ src/data/cars-for-sale.ts
@@ -14,65 +14,15 @@ export type CarsForSaleCard = {
 
 export const carsForSaleInventory: CarsForSaleCard[] = [
   {
-    "car": "2022 Rolls-Royce Ghost Black Badge (White/Black)",
+    "car": "2025 Aston Martin DBX",
     "miles": "61,000",
     "vin": "SCATD6C05NU213792",
     "threeMo": {
-      "ownerDown": 24900,
-      "scgDown": 27900,
-      "ownerMonthly": 5500,
-      "scgMonthly": 5595,
-      "profit": 8595,
-      "buyout": 230000,
-      "scgBuyout": 229900,
-      "buyoutSpread": -100
+      "buyout": 115000
     },
     "twelveMo": {
-      "ownerDown": 44900,
-      "scgDown": 47900,
-      "ownerMonthly": 5500,
-      "scgMonthly": 5595,
-      "profit": 8595,
-      "buyout": 215000,
-      "scgBuyout": 215000,
-      "buyoutSpread": 0
+      "buyout": 115000
     },
-    "milesPerYear": "—",
-    "firstPaymentDueToOwner": "30 days after closing",
-    "location": "New York, NY, United States",
-    "ownerName": "Top 2 Not 2 LLC",
-    "ownerEmail": "stage2autogroup@gmail.com",
-    "notes": "—"
-  },
-  {
-    "car": "2020 Rolls-Royce Cullinan White/Red",
-    "miles": "47,000",
-    "vin": "SLATV4C0XLU201507",
-    "threeMo": {
-      "ownerDown": 24900,
-      "scgDown": 26900,
-      "ownerMonthly": 6900,
-      "scgMonthly": 6900,
-      "profit": 8900,
-      "buyout": 245000,
-      "scgBuyout": 244900,
-      "buyoutSpread": -100
-    },
-    "twelveMo": {
-      "ownerDown": 49900,
-      "scgDown": 49900,
-      "ownerMonthly": 6900,
-      "scgMonthly": 6900,
-      "profit": 6900,
-      "buyout": 229900,
-      "scgBuyout": 229900,
-      "buyoutSpread": 0
-    },
-    "milesPerYear": "—",
-    "firstPaymentDueToOwner": "30 days after closing",
-    "location": "New York, NY, United States",
-    "ownerName": "DAM HOLDINGS LLC",
-    "ownerEmail": "stage2autogroup@gmail.com",
-    "notes": "—"
+    "notes": "\u2014"
   }
 ];
